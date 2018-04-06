be0217593231c3c151a9581b0587f6cf26ab4ccc
Marina-chan <marina.k.chan@gmail.com>
2018-04-06 12:17:12 +0300

    Fixed user REST

--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,6 @@
 import re
-from os import urandom
-from binascii import hexlify
-from hashlib import sha512, sha256
+from hashlib import sha512
+from secrets import token_urlsafe
 
 from flask_restful import Resource, reqparse
 
@@ -16,30 +15,30 @@ mail_validator = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)
 class UserREST(Resource):
 
     def __init__(self):
-        self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('username', required=True)
+        self.reqparse = reqparse.RequestParser(bundle_errors=True)
+        self.reqparse.add_argument('id', type=int, required=True)
         super(UserREST, self).__init__()
 
     def post(self):
         args = self.reqparse.parse_args()
-        if len(args['username']) > 60:
-            return {'error': 'invalid_username'}, 400
-        user = User.query.filter_by(username=args['username']).first()
+        if 'id' not in args:
+            return {'error': 'no_user_id'}, 404
+        user = User.query.filter_by(id=args['id']).first()
         if user:
-            return {'user': user.username, 'mail': user.email}, 200
-        return {'error': 'no_user'}, 400
+            return {'user': user.username}, 200
+        return {'error': 'no_user'}, 404
 
 
 class UserRegisterREST(Resource):
 
     def __init__(self):
-        self.reqparse = reqparse.RequestParser()
+        self.reqparse = reqparse.RequestParser(bundle_errors=True)
         self.reqparse.add_argument('username', required=True)
         self.reqparse.add_argument('user_mail', required=True)
         self.reqparse.add_argument('pwd', required=True)
         super(UserRegisterREST, self).__init__()
 
-    def put(self):
+    def post(self):
         args = self.reqparse.parse_args()
         username, user_mail, pwd_hash = args['username'], args['user_mail'], args['pwd']
         if len(username) > 60:
@@ -53,13 +52,13 @@ class UserRegisterREST(Resource):
         user = User(username=username, email=user_mail, password=pwd_hash)
         db.session.add(user)
         db.session.commit()
-        return {'status': 'created'}, 201
+        return {'user_id': User.query.filter_by(username=username).first().id}, 201
 
 
 class UserAuthorizationREST(Resource):
 
     def __init__(self):
-        self.reqparse = reqparse.RequestParser()
+        self.reqparse = reqparse.RequestParser(bundle_errors=True)
         self.reqparse.add_argument('username', required=True)
         self.reqparse.add_argument('pwd', required=True)
         self.reqparse.add_argument('salt', required=True)
@@ -75,16 +74,20 @@ class UserAuthorizationREST(Resource):
             return {'error': 'no_user'}, 400
         pwd = sha512(f'{user.password}:{salt}'.encode()).hexdigest()
         if pwd == pwd_hash:
-            token = sha256(f'{user.username}:{hexlify(urandom(16)).decode()}'.encode()).hexdigest()
+            token = token_urlsafe(32)
             r[token] = user.username
             r.expire(token, 259200)
-            return {'token': token}, 200
+            return {
+                       'token': token,
+                       'user_id': user.id,
+                       'username': user.username
+                   }, 200
 
 
 class UserTokenAuthorizeREST(Resource):
 
     def __init__(self):
-        self.reqparse = reqparse.RequestParser()
+        self.reqparse = reqparse.RequestParser(bundle_errors=True)
         self.reqparse.add_argument('token', required=True)
         super(UserTokenAuthorizeREST, self).__init__()
 
@@ -92,7 +95,7 @@ class UserTokenAuthorizeREST(Resource):
         args = self.reqparse.parse_args()
         if args['token'] in r:
             username = r[args['token']]
-            token = sha256(f'{username}:{hexlify(urandom(16)).decode()}'.encode()).hexdigest()
+            token = token_urlsafe(32)
             r[token] = username
             r.expire(token, 259200)
             return {'token': token}, 200
