1058665735b72aa22a907814e4def41ba1e32762
Marina-chan <marina.k.chan@gmail.com>
2018-05-03 13:15:05 +0300

    Small fix

--- init_database.py
+++ init_database.py
@@ -8,9 +8,10 @@ from models import User, Regex, Rating
 
 
 if __name__ == '__main__':
-    if not os.path.exists('config.yaml') or os.environ.get('DATABASE_URL', False):
+    if not os.path.exists('config.yaml') or not os.environ.get('DATABASE_URL', False):
         print('Please create config.yaml from config.yaml.example')
         sys.exit(0)
+    print('Database URI: ', DB_URI)
     if database_exists(DB_URI):
         # sys.exit(0)
         drop_database(DB_URI)
